a3fd1f8b443f6a0f1cc606aa0e41ba25580d2a4d
matteoscorcia <matteoscorcia@gmail.com>
2024-05-23 10:06:28 +0200

    feat: better neotree and scroll options

--- .config/nvim/lua/plugins/scroll.lua
+++ .config/nvim/lua/plugins/scroll.lua
@@ -24,15 +24,15 @@ return {
 			})
 
 			require("neoscroll.config").set_mappings({
-				["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } },
-				["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "250" } },
-				["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "450" } },
-				["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "450" } },
+				["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100" } },
+				["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100" } },
+				["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "100" } },
+				["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "100" } },
 				["<C-y>"] = { "scroll", { "-0.10", "false", "100" } },
 				["<C-e>"] = { "scroll", { "0.10", "false", "100" } },
-				["zt"] = { "zt", { "250" } },
-				["zz"] = { "zz", { "250" } },
-				["zb"] = { "zb", { "250" } },
+				["zt"] = { "zt", { "100" } },
+				["zz"] = { "zz", { "100" } },
+				["zb"] = { "zb", { "100" } },
 			})
 		end,
 	},
