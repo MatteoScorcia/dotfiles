00a1bd23507d9c3e5a45cdfac593db0f85a9633e
matteoscorcia <matteoscorcia@gmail.com>
2025-11-20 15:33:44 +0100

    feat: add yazi

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,8 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Set basic indentation to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- Disable relative numbers
 vim.wo.relativenumber = false
 
 -- Disable animations
 vim.g.snacks_animate = false
+
+-- LSP Server to use for Python
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_ruff = "ruff"
